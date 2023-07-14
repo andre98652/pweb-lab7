cbbe9e27ad05a312c71d0aa491ee7542a69d1511
Andre Delgado <adelgadoal@unsa.edu.pe>
2023-07-13 21:15:25 -0500

    views2

--- laboratorio/laboratorio/urls.py
+++ laboratorio/laboratorio/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from .views import GeneratePdf
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'^pdf/$',GeneratePdf.as_view()),
 ]
