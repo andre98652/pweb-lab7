27bec863331b961a775039837860908a3b5d2af3
Andre Delgado <adelgadoal@unsa.edu.pe>
2023-07-14 11:25:06 -0500

    creacion de tabla movies

--- ModelRelation/example/models.py
+++ ModelRelation/example/models.py
@@ -10,3 +10,18 @@ class Framework(models.Model):
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
+    
+
+class Movie(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class Character(models.model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+    
+    
